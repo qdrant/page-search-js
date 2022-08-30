86bd778e7981ce3aa0555d4e4ab3684d265eb04c
trean <trean.mi@gmail.com>
2022-08-30 18:09:38 +0200

    added build config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: {
+      search: './src/js/search.js',
+      scroll: './src/js/result-scroll.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist/js'),
+      filename: '[name].min.js'
+    },
+  }, {
+
+    entry: './src/scss/styles.scss',
+    output: {
+      path: path.resolve(__dirname, 'dist/css'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: { outputPath: './', name: '[name].min.css'}
+            },
+            'sass-loader'
+          ],
+        },
+      ],
+    },
+  }
+];
